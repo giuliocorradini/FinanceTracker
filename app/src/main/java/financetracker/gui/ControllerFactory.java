200e4d60b6ae56699ae137fcf2222bcc28d27199
giuliocorradini <39673120+giuliocorradini@users.noreply.github.com>
2023-02-16 21:44:57 +0100

    documentation for financetracker.gui high level

--- app/src/main/java/financetracker/gui/ControllerFactory.java
+++ app/src/main/java/financetracker/gui/ControllerFactory.java
@@ -5,14 +5,26 @@ import financetracker.gui.controller.ControllerInjectable;
 import financetracker.gui.controller.ModelInjectable;
 import financetracker.gui.model.BalanceModel;
 
+/**
+ * Factory of controllers. Provides a way of instatiating a controller and inject the application
+ * model or the main controller (an instance of {@link AppController} to achieve the MVC pattern.
+ *
+ * <p>
+ *     A model is injected if the specified controller class implements the {@link ModelInjectable}
+ *     interface.
+ *     The main AppController is injected if the class implements {@link ControllerInjectable}.
+ *     This is used by secondary windows that want to send an event to the main window.
+ *     See {@link financetracker.gui.controller.SearchController} for a use example.
+ * </p>
+ */
 public class ControllerFactory {
 
-    /*
+    /**
      * Generic method that instantiate a Controller class, and is called when a view is created.
      * Called by the custom FXML loader.
      * Each controller is tied to a view in its FXML file.
-     * @param cont_class: the class to be instantiated
-     * @return: a T object of the specified class
+     * @param cont_class the class to be instantiated
+     * @return a T object of the specified class
      */
     public static <T> T buildController(Class<T> cont_class, BalanceModel model, AppController app) {
         //T cont = new T();
@@ -37,6 +49,9 @@ public class ControllerFactory {
         return cont;
     }
 
+    /**
+     * See {@link #buildController(Class, BalanceModel, AppController)}.
+     */
     public static <T> T buildController(Class<T> cont_class, BalanceModel model) {
         return buildController(cont_class, model, null);
     }
--- app/src/main/java/financetracker/gui/ViewLoader.java
+++ app/src/main/java/financetracker/gui/ViewLoader.java
@@ -6,7 +6,17 @@ import javafx.util.Callback;
 
 import java.io.IOException;
 
+/**
+ * This class provides a single static method that helps with FXML file instantiation.
+ */
 public class ViewLoader {
+    /**
+     * Load a view from an FXML file, and override the controller creation by providing a
+     * custom factory method for controller linking. See {@link ControllerFactory}.
+     * @param viewFilePath the FXML file path.
+     * @param controllerFactory a callback for controller instantiation.
+     * @return a loaded view, with nodes from the FXML file.
+     */
     public static Parent load(String viewFilePath, Callback<Class<?>, Object> controllerFactory) {
         Parent parent = null;
 
