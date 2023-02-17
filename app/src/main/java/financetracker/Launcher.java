3583689b929e5f09ba73c47e91fd9847205e697c
giuliocorradini <39673120+giuliocorradini@users.noreply.github.com>
2023-02-17 15:05:01 +0100

    added doc for entrypoint

--- app/src/main/java/financetracker/Launcher.java
+++ app/src/main/java/financetracker/Launcher.java
@@ -1,5 +1,10 @@
 package financetracker;
 
+/**
+ * This is a fake entry point for the application, but it is needed
+ * when packaging everything in a uber-jar file, otherwise the application
+ * won't start.
+ */
 public class Launcher {
     public static void main(String[] args) {
         App.main(args);
